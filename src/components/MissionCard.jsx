02cc92af86cfd8a15606e4b5668ff9d0cbd47344
caue peruque <caue19052003@gmail.com>
2023-03-30 18:32:31 -0300

    css: modifying missiong card

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,36 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './MissionCard.css';
 
 export default class MissionCard extends Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
+      <div data-testid="mission-card" className="mission__card">
         <p
           data-testid="mission-name"
-          className="page__text-font .planetcard__text-weight"
+          className="page__text-font missioncard__text-title missioncard__text-weight"
         >
           {name}
         </p>
-        <p
-          data-testid="mission-year"
-          className="page__text-font .planetcard__text-weight"
-        >
-          {year}
-        </p>
-        <p
-          data-testid="mission-country"
-          className="page__text-font .planetcard__text-weight"
-        >
-          {country}
-        </p>
-        <p
-          data-testid="mission-destination"
-          className="page__text-font .planetcard__text-weight"
-        >
-
-          {destination}
-        </p>
+        <div className="missioncard__container-info">
+          <div className="missioncard__container-year-country">
+            <p
+              data-testid="mission-year"
+              className="page__text-font missioncard__text-weight"
+            >
+              {year}
+            </p>
+            <p
+              data-testid="mission-country"
+              className="page__text-font missioncard__text-weight"
+            >
+              {country}
+            </p>
+          </div>
+          <div className="mission__container-destination">
+            <p
+              data-testid="mission-destination"
+              className="page__text-font missioncard__text-weight"
+            >
+              {destination}
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
