7c26e449f75eee13f18018e36966c97c57cea51f
caue peruque <caue19052003@gmail.com>
2023-03-30 15:18:14 -0300

    requisito 10 finalizado

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 export default class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions"><Title headline="Missões" /></div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {missions.map((mission) => (
+          <MissionCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />))}
+      </div>
     );
   }
 }
